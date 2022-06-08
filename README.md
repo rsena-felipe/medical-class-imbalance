ae1afbced841943a92ae0f03eaf5997c419fceba
rsena-felipe <raulfelipe.sena@gmail.com>
2022-06-08 18:14:12 +0200

    Original File Titanic

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hangling Class Imbalance with medical data 
+
+
+
+## Project Structure
+
+The project is structured as follows: 
+
+```bash
+
+     
+```        
+
+## Content
+
---
+++ src/my_functions/__init__.py
@@ -0,0 +1,1 @@
+from .evaluate import calculate_f1score, calculate_satisfying_metric, make_predictions, save_predictions, plot_confusion_matrices, plot_classification_reports, print_accuracies, plot_roc_curve
